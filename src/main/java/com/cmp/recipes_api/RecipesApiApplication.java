6c4153ccaff2d9487ef62e88509849b9c3d6db67
ELCANET\cmp <carmen.molineropena@elca.ch>
2024-07-12 11:31:52 +0200

    testing commit

--- src/main/java/com/cmp/recipes_api/RecipesApiApplication.java
+++ src/main/java/com/cmp/recipes_api/RecipesApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecipesApiApplication {
 
 	public static void main(String[] args) {
+		// testing commit
 		SpringApplication.run(RecipesApiApplication.class, args);
 	}
 
